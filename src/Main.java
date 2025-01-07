c582ae05fd3d049c0656b25528391c13f8c0a17d
Daniel Indrieri <indrieridaniel@gmail.com>
2025-01-07 14:17:18 +0100

    first

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 4;
+            System.out.println(x + y);
+
+        int a = 2;
+        String c = "ciao";
+            System.out.println(a + c);
+    }
+}
